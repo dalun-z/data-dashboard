f20bd845e6c8499806d327de8d5f6962d693f54e
dzhang1238@gmail.com <dzhang1238@gmail.com>
2024-01-23 22:19:24 -0800

    pre-setup project;

---
+++ src/app/components/SideMenu/SideMenu.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SideMenu = () => {
+    return (
+        <ul>
+            <li>Home</li>
+            <li>Profile</li>
+            <li>Setting</li>
+            <li>Something</li>
+        </ul>
+    )
+}
+
+export default SideMenu;
---
+++ src/app/dashboard/data/Data.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Data = () => {
+    return (
+        <h1> Data page </h1>
+    )
+}
+
+export default Data;
